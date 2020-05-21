7aafb21bf226012bdca0869c347885c61052f3cd
Vlad <asdzorg@yandex.ru>
2020-05-22 01:16:40 +0300

    registration and error messages

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    error: null
   },
   mutations: {
+    setError(state, error) {
+      //debugger
+      state.error = error
+    },
+    clearError(state) {
+      state.error = null
+    }
+  },
+  getters: {
+    error: s => s.error
   },
   actions: {
   },
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -93,7 +93,7 @@
       agree: {checked: v => v}
     },
     methods: {
-      submitHandler() {
+      async submitHandler() {
         if(this.$v.$invalid) {
           this.$v.$touch()
           return
@@ -105,9 +105,11 @@
           name: this.name
         }
 
-        console.log(formData)
+        try {
+          await this.$store.dispatch('register', formData)
+          this.$router.push('/')
+        } catch (e) {}
 
-        this.$router.push('/')
       }
     }
   }
