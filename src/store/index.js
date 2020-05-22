fc180b0669a4ec512ee32d7a0c70a0b68a8d0300
Vlad <asdzorg@yandex.ru>
2020-05-23 00:06:54 +0300

    fetch info

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
+import info from './info'
 
 Vue.use(Vuex)
 
@@ -23,6 +24,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-    auth
+    auth, info
   }
 })
