7dcb067bb544fd2f8e86106cefb35ae92b15fc3c
Vlad <asdzorg@yandex.ru>
2020-05-30 20:03:11 +0300

    routes protection and err in mainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -25,6 +25,7 @@
 <script>
   import Navbar from '@/components/app/Navbar'
   import Sidebar from '@/components/app/Sidebar'
+  import messages from '@/utils/messages'
 
   export default {
     name: 'main-layout',
@@ -40,7 +41,16 @@
     },
     components: {
       Navbar, Sidebar
+    },
+    computed: {
+      error() {
+        return this.$store.getters.error
+      }
+    },
+    watch: {
+      error(fbError) {
+        this.$error(messages[fbError.code] || 'Что-то пошло не так')
+      }
     }
-
   }
 </script>
